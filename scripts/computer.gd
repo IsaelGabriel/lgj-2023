19ce34fb94feedf1fbeabfddf3de18bebf323292
Felipe <fpachecomanoel@uol.com.br>
2023-08-26 16:56:53 -0300

    changed virus flood algorythm to slow spread speed

--- scripts/computer.gd
+++ scripts/computer.gd
@@ -5,26 +5,31 @@ enum Status {FREE,BUSY,VIRUS}
 @onready var connections=[]
 @onready var status=Status.FREE
 @onready var virus_timer=$VirusTimer
+@onready var sprite=$Sprite2D
 func start(adjacency):
 	connections=adjacency
 
-func interact(): #temporary for testing
+func use(): #temporary for testing
 	if status==Status.VIRUS:
 		fix()
 	elif status==Status.FREE:
 		virus()
 func virus():
-	status=Status.VIRUS
-	virus_timer.start()
+	if status!=Status.VIRUS:
+		status=Status.VIRUS
+		virus_timer.start()
+		sprite.frame=1
 
 func infect():
-	for i in connections:
-		i.virus()
+	randomize()
+	var i=randi()%(connections.size())
+	connections[i].virus()
 
 func fix():
 	if status==Status.VIRUS:
 		status=Status.FREE
 		virus_timer.stop()
+		sprite.frame=0
 	
 func _on_virus_timer_timeout():
-	virus()
+	infect()
