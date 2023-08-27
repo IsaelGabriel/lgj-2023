e96f5a248aea1447353b8f6cbc4f433c8c44bdca
Isael <isaelgsfaria@gmail.com>
2023-08-27 00:24:50 -0300

    fix player walk animation bug by basing state on velocity

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 # constants
 
 const SPEED = 100.0
+const ACC = 120.0
 
 # enums
 
@@ -14,15 +15,41 @@ const SPEED = 100.0
 @onready var animator = $AnimationPlayer
 @onready var selector = $Selector
 var current_target = null
+var acceleration: Vector2 = Vector2(0, 0)
+var decceleration: Vector2 = Vector2(0, 0)
 
-func _process(_delta):
-	handle_input()
 
-func _physics_process(_delta):
+func _process(delta):
+	handle_input(delta)
+
+func _physics_process(delta):
+	
+	velocity += acceleration * delta
+	if acceleration.x != 0.0 and acceleration.y != 0.0:
+		velocity.x = clamp(velocity.x, -SPEED * 0.7, SPEED * 0.7)
+		velocity.y = clamp(velocity.y, -SPEED * 0.7, SPEED * 0.7)
+	else:
+		velocity.x = clamp(velocity.x, -SPEED, SPEED)
+		velocity.y = clamp(velocity.y, -SPEED, SPEED)
+		
+	velocity += decceleration * delta
+	
+	if decceleration.x < 0:
+		velocity.x = clamp(velocity.x, 0.0, SPEED)
+	elif decceleration.x > 0:
+		velocity.x = clamp(velocity.x, -SPEED, 0.0)
+		
+	if decceleration.y < 0:
+		velocity.y = clamp(velocity.y, 0.0, SPEED)
+	elif decceleration.y > 0:
+		velocity.y = clamp(velocity.y, -SPEED, 0.0)
+	
+	animator.speed_scale = clamp(velocity.length(), 0.7, SPEED) / SPEED
+	
 	move_and_slide()
 	handle_selection()
 
-func handle_input():
+func handle_input(delta):
 	var x_input = 0.0
 	if Input.is_action_pressed("p1_left"):
 		x_input += -1
@@ -39,7 +66,7 @@ func handle_input():
 		y_input += 1
 		current_direction = Vector2.DOWN
 	
-	if x_input == 0.0 and y_input == 0.0:
+	if velocity.x == 0 and velocity.y == 0:
 		match current_direction:
 			Vector2.DOWN:
 				animator.play("player_idle_down")
@@ -70,7 +97,23 @@ func handle_input():
 		x_input *= 0.7
 		y_input *= 0.7
 	
-	velocity = Vector2(x_input, y_input) * SPEED
+	acceleration = Vector2(x_input, y_input) * ACC
+	decceleration = Vector2(0.0, 0.0)
+	
+	if x_input == 0:
+		if velocity.x > 0:
+			decceleration.x = -ACC
+		if velocity.x < 0:
+			decceleration.x = ACC
+	
+	if y_input == 0:
+		if velocity.y > 0:
+			decceleration.y = -ACC
+		if velocity.y < 0:
+			decceleration.y = ACC
+	
+	if decceleration.x != 0 and decceleration.y != 0:
+		decceleration *= 0.7
 	
 	if Input.is_action_just_pressed("p1_use") and current_target != null:
 		current_target.interacting = not current_target.interacting
